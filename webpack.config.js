01956376aa244aa91625750ebdb53f341eda4cd2
Diego González Prado <dpradodev@gmail.com>
2023-10-31 00:27:52 +0100

    fixed tailwind config and added header component

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,21 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.scss$/,
-        loader: 'postcss-loader',
-        options: {
-          postcssOptions: {
-            ident: 'postcss',
-            syntax: 'postcss-scss',
-            plugins: [
-              require('postcss-import'),
-              require('tailwindcss'),
-              require('autoprefixer'),
-            ],
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                ident: 'postcss',
+                plugins: [
+                  require('tailwindcss'),
+                  require('autoprefixer')
+                ],
+              },
+            },
           },
-        },
+        ],
       },
     ],
   },
